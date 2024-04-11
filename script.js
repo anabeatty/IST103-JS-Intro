9e280f677049819b426eff560c9e14d518ac71ef
“anabeatty” <anabmdeazevedo@gmail.com>
2024-04-10 22:15:32 -0700

    trying

---
+++ script.js
@@ -0,0 +1,60 @@
+alert("Be ready");
+let person_name = "Taylor Swift";
+let person_full_name = "Taylor Alison Swift";
+let person_birthday = "10/13/1989";
+let person_hometown = "West Reading, Pennsylvania";
+let person_occupations = [
+  "Singer-songwriter",
+  "Producer",
+  "Director",
+  "Businesswoman",
+  "Actress",
+];
+let albums = [
+  "Taylor Swift",
+  "Fearless",
+  "Speak Now",
+  "Red",
+  "1989",
+  "Reputation",
+  "Lover",
+  "Folklore",
+  "Evermore",
+  "Midnights",
+  "The Tortured Poets Department",
+];
+let genres = ["Pop", "Country", "Folk", "Rock", "Alternative"];
+let instruments = ["vocals", "guitar", "piano", "banjo", "ukulele"];
+let awards_wins = 1205;
+let awards_nominations = 1809;
+let write_songs_about_exs = true;
+let is_she_famous_because_KW = false;
+let number_of_sisters = null;
+let bad_album = undefined;
+let her_scarf = {
+  color: "red",
+  length: 60,
+  location: "Ex's sister's house",
+  material: "cashmere",
+};
+
+const new_album_release_date = "Apr 19, 2024";
+
+//console.log of the variables;
+console.log("Person's name:", person_name);
+console.log("Person's full name:", person_full_name);
+console.log("Person's birthday:", person_birthday);
+console.log("Person's hometown:", person_hometown);
+console.log("Occupations:", person_occupations);
+console.log("Albums:", albums);
+console.log("Genres:", genres);
+console.log("Instruments:", instruments);
+console.log("Awards Wins:", awards_wins);
+console.log("Awards Nominations:", awards_nominations);
+console.log("Write songs about exs?", write_songs_about_exs);
+console.log("Is she famous because K.W.?", is_she_famous_because_KW);
+console.log("Number of sisters:", number_of_sisters);
+console.log("Bad Album:", bad_album);
+console.log("Her scarf:", her_scarf);
+
+console.log("New Album Release Date:", new_album_release_date);
